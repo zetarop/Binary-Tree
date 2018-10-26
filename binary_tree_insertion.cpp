0f8eb746f9ff62654702f35cdccd02f9843555c9
zetarop <arkupa199888@gmail.com>
2018-10-26 10:54:28 +0530

    Create binary_tree_insertion.cpp

---
+++ binary_tree_insertion.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define ve vector
+#define pb push_back
+#define ll long long
+using namespace std;
+
+class node
+{
+public:
+	int val;
+	node* l;
+	node* r;
+};
+
+node* newNode(int n)
+{
+	node* temp = new node;
+	temp->val=n;
+	temp->l=NULL;
+	temp->r=NULL;
+	return temp;
+}
+
+node* insert(node* root, int n)
+{
+	if(root == NULL)
+		return newNode(n);
+	if(n <= root->val)
+		root->l = insert(root->l, n);
+	else
+		root->r = insert(root->r, n);
+}
+
+void inOrder(node* root)
+{
+	if(root->l != NULL)
+		inOrder(root->l);
+
+	cout<<root->val<<" ";
+
+	if(root->r != NULL)
+		inOrder(root->r);
+}
+
+int main()
+{
+	node* root=NULL;
+	ve<int> v{50,40,60,30,45,55,70,20,35,48,65,80,10,25,33,38,75,90,71,85,100,78};
+	for(auto a:v)
+		root = insert(root,a);
+	inOrder(root);
+}
